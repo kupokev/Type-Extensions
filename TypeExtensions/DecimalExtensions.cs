c9d7a585632828f8b0e77aabd4d4da41bcafd688
Kevin Williams <kupo.kev@gmail.com>
2019-09-20 12:08:41 -0500

    added truncate for double and decimal

---
+++ TypeExtensions/DecimalExtensions.cs
@@ -0,0 +1,32 @@
+﻿public static class DecimalExtensions
+{
+    public static decimal Truncate(this decimal value, int precision)
+    {
+        var amount = value.ToString();
+
+        if (amount.Contains(".") && amount.Length - amount.IndexOf(".") > precision)
+        {
+            return decimal.Parse(amount.Substring(0, amount.IndexOf(".") + precision + 1));
+        }
+        else
+        {
+            return value;
+        }
+    }
+
+    public static decimal? Truncate(this decimal? value, int precision)
+    {
+        if (value == null) return null;
+
+        var amount = value.ToString();
+
+        if (amount.Contains(".") && amount.Length - amount.IndexOf(".") > precision)
+        {
+            return decimal.Parse(amount.Substring(0, amount.IndexOf(".") + precision + 1));
+        }
+        else
+        {
+            return value;
+        }
+    }
+}
---
+++ TypeExtensions/DoubleExtensions.cs
@@ -0,0 +1,32 @@
+﻿public static class DoubleExtensions
+{
+    public static double Truncate(this double value, int precision)
+    {
+        var amount = value.ToString();
+
+        if (amount.Contains(".") && amount.Length - amount.IndexOf(".") > precision)
+        {
+            return double.Parse(amount.Substring(0, amount.IndexOf(".") + precision + 1));
+        }
+        else
+        {
+            return value;
+        }
+    }
+
+    public static double? Truncate(this double? value, int precision)
+    {
+        if (value == null) return null;
+
+        var amount = value.ToString();
+
+        if (amount.Contains(".") && amount.Length - amount.IndexOf(".") > precision)
+        {
+            return double.Parse(amount.Substring(0, amount.IndexOf(".") + precision + 1));
+        }
+        else
+        {
+            return value;
+        }
+    }
+}
