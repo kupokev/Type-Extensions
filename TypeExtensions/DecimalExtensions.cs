49ec997551991adac5447a048915df3791c4b55e
Kevin Williams <kupo.kev@gmail.com>
2019-10-15 18:28:00 -0500

    updated truncate to be trunc so it will work in vs2017

--- TypeExtensions/DecimalExtensions.cs
+++ TypeExtensions/DecimalExtensions.cs
@@ -1,23 +1,23 @@
-﻿using System;
+﻿using static System.Math;
 
 namespace TypeExtensions
 {
     public static class DecimalExtensions
     {
-        public static decimal Truncate(this decimal value, int precision)
+        public static decimal Trunc(this decimal value, int precision)
         {
-            var amount = (decimal)Math.Pow(10, precision);
+            var amount = (decimal)Pow(10, precision);
 
-            return Math.Truncate(value * amount) / amount;
+            return Truncate(value * amount) / amount;
         }
 
-        public static decimal? Truncate(this decimal? value, int precision)
+        public static decimal? Trunc(this decimal? value, int precision)
         {
             if (value == null) return null;
 
-            var amount = (decimal)Math.Pow(10, precision);
+            var amount = (decimal)Pow(10, precision);
 
-            return Math.Truncate((decimal)value * amount) / amount;
+            return Truncate((decimal)value * amount) / amount;
         }
     }
 }
--- TypeExtensions/DoubleExtensions.cs
+++ TypeExtensions/DoubleExtensions.cs
@@ -1,23 +1,23 @@
-﻿using System;
+﻿using static System.Math;
 
 namespace TypeExtensions
 {
     public static class DoubleExtensions
     {
-        public static double Truncate(this double value, int precision)
+        public static double Trunc(this double value, int precision)
         {
-            var amount = Math.Pow(10, precision);
+            var amount = Pow(10, precision);
 
-            return Math.Truncate(value * amount) / amount;
+            return Truncate(value * amount) / amount;
         }
 
-        public static double? Truncate(this double? value, int precision)
+        public static double? Trunc(this double? value, int precision)
         {
             if (value == null) return null;
 
-            var amount = Math.Pow(10, precision);
+            var amount = Pow(10, precision);
 
-            return Math.Truncate((double)value * amount) / amount;
+            return Truncate((double)value * amount) / amount;
         }
     }
 }
